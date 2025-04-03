2bee071ada8fab8efe19a0be97b07846beb1fecb
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-03 10:11:19 -0400

    updated main text on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,25 @@ export default function Home() {
         <Container className="main-content">
           <Row className="shadow-lg mt-3 p-3 bg-body rounded">
             <Col lg={7}>
-              Welcome to NextDex, your ultimate destination for exploring the
-              vast world of Pokémon! Built with cutting-edge web technologies
-              including React, Next.js, and Bootstrap, NextDex offers a fast,
-              responsive, and visually appealing experience for trainers of all
-              levels. Dive into our comprehensive Pokédex section to search,
-              filter, and learn intricate details about every known Pokémon.
-              Beyond the Pokédex, NextDex is designed to be an ever-expanding
-              resource, featuring in-depth information on Pokémon games,
-              regions, items, characters, and much more. Prepare to enhance your
-              Pokémon knowledge with a sleek interface and a wealth of data
-              right at your fingertips!
+              <h2 className="fw-bold mb-3 text-center">Welcome to NextDex!</h2>
+              <p className="lead fs-6">
+                Your ultimate destination for exploring the vast world of
+                Pokémon!
+              </p>
+              <p className="lh-lg">
+                Built with cutting-edge web technologies including{" "}
+                <strong>React</strong>, <strong>Next.js</strong>, and{" "}
+                <strong>Bootstrap</strong>, NextDex offers a fast, responsive,
+                and visually appealing experience for trainers of all levels.
+                Dive into our comprehensive <strong>Pokédex</strong> section to
+                search, filter, and learn intricate details about every known{" "}
+                <strong>Pokémon</strong>. Beyond the Pokédex, NextDex is
+                designed to be an ever-expanding resource, featuring in-depth
+                information on <strong>Pokémon</strong> games, regions, items,
+                characters, and much more. Prepare to enhance your{" "}
+                <strong>Pokémon</strong> knowledge with a sleek interface and a
+                wealth of data right at your fingertips!
+              </p>
             </Col>
             <Col lg={5}>
               <TypesSection />
