d2245aa86ed2a5cecf4b7ec2c22cff5b445aae29
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-04-03 10:04:23 -0400

    Added Type section to index page

---
+++ components/pokemon/TypesSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { Badge, Col, Row } from "react-bootstrap";
+import typesData from "@/json/pokemon/types.json";
+import { PokemonType } from "@/types/pokemon/Types";
+
+function TypesSection() {
+  const types: PokemonType[] = typesData ?? [];
+
+  return (
+    <Row g={2}>
+      {types.map((type) => (
+        <Col key={type.name} xs={4} lg={2} className="mb-2 px-1">
+          <a href={`#${type.name}`} style={{ textDecoration: "none" }}>
+            <Badge
+              className="w-100 d-flex align-items-center justify-content-center"
+              style={{
+                backgroundColor: `${type.color} !important`,
+                color: "#fff",
+                textShadow: "1px 1px 2px rgba(0,0,0,0.6)",
+                padding: "0.5em !important",
+              }}
+            >
+              {type.imgSrc && (
+                <img
+                  src={type.imgSrc}
+                  alt={`${type.name} type icon`}
+                  style={{
+                    height: "1.0em",
+                    marginRight: "0.3em",
+                  }}
+                />
+              )}
+              <span>{type.name}</span>{" "}
+            </Badge>
+          </a>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default TypesSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
+//Components
 import MonHeader from "@/components/MonHeader";
 import MonFooter from "@/components/MonFooter";
+import TypesSection from "@/components/pokemon/TypesSection";
 
 export default function Home() {
   return (
@@ -9,7 +11,7 @@ export default function Home() {
         <MonHeader showBadge={true} />
         <Container className="main-content">
           <Row className="shadow-lg mt-3 p-3 bg-body rounded">
-            <Col lg={8} className="mx-auto">
+            <Col lg={7}>
               Welcome to NextDex, your ultimate destination for exploring the
               vast world of Pokémon! Built with cutting-edge web technologies
               including React, Next.js, and Bootstrap, NextDex offers a fast,
@@ -21,6 +23,9 @@ export default function Home() {
               regions, items, characters, and much more. Prepare to enhance your
               Pokémon knowledge with a sleek interface and a wealth of data
               right at your fingertips!
+            </Col>
+            <Col lg={5}>
+              <TypesSection />
             </Col>
           </Row>
         </Container>
