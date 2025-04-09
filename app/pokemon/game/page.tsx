803135d1a684aa6ff21aecdba3ee36f5876d4213
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-04-09 14:01:54 +0000

    Converted over the pokemon game page

--- app/pokemon/game/page.tsx
+++ app/pokemon/game/page.tsx
@@ -3,15 +3,15 @@ import { Container, Row } from "react-bootstrap";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Pokémon",
+  title: "Pokémon Game",
 };
 
-export default function PokemonPage() {
+export default function PokemonGamePage() {
   return (
     <Container className="main-content mt-3 mb-3">
       <Row className="shadow-lg mt-3 p-3 bg-body rounded">
         <Row className="mt-5">
-          <h2 className="text-center">Pokémon</h2>
+          <h2 className="text-center">Pokémon Game</h2>
           <span className="text-center">Coming Soon.....</span>
         </Row>
       </Row>
